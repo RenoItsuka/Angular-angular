d7099fab369bb3324277630e286f7f8a9472d9cc
IE Soluciones 2 <iesoluciones2@Mac-mini-de-IE-Soluciones-2.local>
2021-07-09 17:20:40 -0600

    Cierre de este ejercicio de Angular

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -8,10 +8,12 @@ import { Component } from "@angular/core";
 })
 export class PersonasComponent{
     deshabilitar = false;
-    mensaje = 'No se ha agregado ninguna persona';
+    mensaje = '';
     titulo = 'Ingeniero';
+    mostrar = false;
 
     agregarPersona(){
+        this.mostrar = true;
         this.mensaje = 'Persona agregada';
     }
 
